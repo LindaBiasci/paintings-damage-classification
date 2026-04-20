effe8a4103601d1f4c054b8fe39ecb0bfcb179d5
Linda Biasci <lindabi2002@gmail.com>
2026-04-20 18:21:30 +0200

    improved python scripts using pylint

--- python/CNN_feature_extractor.py
+++ python/CNN_feature_extractor.py
@@ -4,43 +4,60 @@ to extract deep feature representations from an image dataset.
 Output: a csv file containing 2048-dimensional feature vectors and their
 corresponding class labels."""
 
+from pathlib import Path
 import os
-os.environ["KERAS_BACKEND"] = "torch"
-import keras
 import numpy as np
 import pandas as pd
-from pathlib import Path
+# Required before importing Keras modules
+os.environ["KERAS_BACKEND"] = "torch"
 from keras.applications.resnet50 import ResNet50, preprocess_input
 from keras.utils import load_img, img_to_array
 
 # Configuration
-INPUT_FOLDER = Path("C:/Users/linda/Desktop/materiali università/magistrale/Computing methods for experimental physics/project_dataset/processed/unpaired")
-OUTPUT_CSV = Path("C:/Users/linda/Desktop/materiali università/magistrale/Computing methods for experimental physics/project_dataset/features_cnn.csv")
+INPUT_FOLDER = Path(
+    "C:/Users/linda/Desktop/materiali università/magistrale/Computing methods for experimental physics/project_dataset/processed/unpaired")
+OUTPUT_CSV = Path(
+    "C:/Users/linda/Desktop/materiali università/magistrale/Computing methods for experimental physics/project_dataset/features_cnn.csv")
 IMAGE_SIZE = (224, 224)
 LABEL_MAP = {"damaged": 0, "undamaged": 1}
+
+def extract_image_data(path):
+    """Load and preprocess a single image for ResNet50.
+    Args:
+        path (Path): Pathlib object pointing to the image file
+    Returns:
+        np.array: preprocessed image data (if successful)
+    """
+
+    try:
+        img = load_img(path, target_size=IMAGE_SIZE)
+        # Convert image to array and apply ResNet50 specific preprocessing
+        return preprocess_input(img_to_array(img))
+    except (OSError, ValueError):
+        print(f"Skipping invalid image: {path.name}")
+        return None
 
 def main():
     """Main execution pipeline for deep feature extraction.
     Load a pretrained ResNet50 model removing the final classification layer, 
     load and preprocess images from dataset, extract deep features using forward pass, 
-    associate features with class labels, save results to csv for ML classification."""
-    
+    associate features to class labels, save results to csv for ML classification."""
+
     # Load pre-trained CNN, remove classification layer, get a flat vector of 2048 features
     model = ResNet50(weights="imagenet", include_top=False, pooling="avg")
 
-    # Load and preprocess images to match the model input format
+    # Find all images in the input folder
     image_paths = list(INPUT_FOLDER.rglob("*.jpg"))
     print(f"Found {len(image_paths)} images.")
-    images = np.array([img_to_array(load_img(p, target_size=IMAGE_SIZE)) for p in image_paths])
-    x = preprocess_input(images)
+    # Load and process each image into a pixel matix, store it and filter out corrupted files
+    x = np.array([data for p in image_paths if (data := extract_image_data(p)) is not None])
 
-    # Feature extraction
-    features = model.predict(x, verbose='true')
+    # Actual feature extraction
+    features = model.predict(x)
 
-    # Build dataset, i.e. merge feature vectors with their respective class labels
-    df = pd.DataFrame(features, columns=[f"feat_{i}" for i in range(features.shape[1])])
-    df["Label"] = [p.parent.name for p in image_paths]
-    df["Label"] = df["Label"].map(LABEL_MAP)
+    # Build dataset, i.e. map feature vectors to their correspondent class label
+    df = pd.DataFrame(features).add_prefix("feat_")
+    df["Label"] = [LABEL_MAP[p.parent.name] for p in image_paths]
 
     # Check for unmapped labels
     if df["Label"].isnull().any():
